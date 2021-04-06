20158690c4f0b688f4d8e944426cc2bc71a2d30a
alvedge <alvedge@gmail.com>
2021-04-06 05:50:50 +0200

    Screen Pixel Coordinates
    FVector2D & GetViewportSize(ViewportSizeX, ViewportSizeY); are the new topics

--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -31,4 +31,10 @@ private:
 
 // Return an OUT parameter, true if hit landscape
 	bool GetSightRayHitLocation(FVector& HitLocation) const;
+
+UPROPERTY(EditAnywhere)
+	float CrosshairXLocation = 0.5;
+
+UPROPERTY(EditAnywhere)
+	float CrosshairYLocation = 0.3333;
 };
